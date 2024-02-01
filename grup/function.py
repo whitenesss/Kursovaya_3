258ee7b5616b85402f476a17d936e643fd55ad00
Purenkov Alex <www.wolk94@gmail.com>
2024-02-01 16:16:48 +0300

    ready file function

--- grup/function.py
+++ grup/function.py
@@ -42,21 +42,76 @@ def transaction_data(kol_vo=5):
 
 
 
-print(*transaction_data(2))
-qwe = transaction_data(2)[0]['operationAmount']['amount']
-print(qwe)
-
-
-
+ewq = transaction_data(2)[0]
+tre = transaction_data(2)[1]
 #
-# # def print_display_to_user():
-# print(transaction_data(2)[0]['date'])
-# dat = transaction_data(2)[0]['date']
-# translation_description = transaction_data(2)[0]['description']
-#
-# dat = dat[0:10].split("-")
-# print(f'{dat[2]}.{dat[1]}.{dat[0]} {translation_description}')
+# print(ewq)
+# print(tre['from'])
+def checking_cards_from(kol_v):
+    '''
+    We collect the sender's data, card, account, bank name and encrypt
+    '''
 
 
+    if 'from' in kol_v:
+
+
+
+        card = kol_v['from']
+        card_1 = card.split()
+        card_number = card_1[-1]
+        if len(card_1) == 3:
+            card_name = card_1[0:2]
+        elif len(card_1) == 2:
+            card_name = card_1[0:1]
+
+        if 16 == len(card_number):
+            private_number1 = card_number[:6] + (len(card_number[6:-4]) * '*') + card_number[-4:]
+            chunks, chunk_size = len(private_number1), len(private_number1) // 4
+            private_number = " ".join([private_number1[i:i + chunk_size] for i in range(0, chunks, chunk_size)])
+            return *card_name, private_number
+        elif 20 == len(card_number):
+            private_number1 = card_number[:6] + (len(card_number[6:-4]) * '*') + card_number[-4:]
+            chunks, chunk_size = len(private_number1), len(private_number1) // 4
+            private_number = " ".join([private_number1[i:i + chunk_size] for i in range(0, chunks, chunk_size)])
+            return *card_name, private_number
+    return "отсутствует"
+
+def checking_cards_to(kol_v):
+
+    if 'to' in kol_v:
+
+
+
+        card = kol_v['to']
+        card_1 = card.split()
+        card_number = card_1[-1]
+        if len(card_1) == 3:
+            card_name = card_1[0:2]
+        elif len(card_1) == 2:
+            card_name = card_1[0:1]
+
+        if 16 == len(card_number):
+            private_number1 = '**' + card_number[-4:]
+            return *card_name, private_number1
+        elif 20 == len(card_number):
+            private_number1 = '**' + card_number[-4:]
+            return *card_name, private_number1
+    return "отсутствует"
+
+
+
+
+
+
+def print_display_to_user(kolvo=5, iter=None):
+    dat = transaction_data(kolvo)[iter]['date']
+    translation_description = transaction_data(kolvo)[iter]['description']
+
+    dat = dat[0:10].split("-")
+    print(f'{dat[2]}.{dat[1]}.{dat[0]} {translation_description}')
+    print(*checking_cards_from(transaction_data(kolvo)[iter]), '->', *checking_cards_to(transaction_data(kolvo)[iter]))
+    print(f"{transaction_data(kolvo)[iter]['operationAmount']['amount']}"
+          f"{transaction_data(kolvo)[iter]['operationAmount']['currency']['name']}")
 
 
